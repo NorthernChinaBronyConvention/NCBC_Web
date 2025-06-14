6a864a88144938ce515008a4d22f9fe19e0ab56e
JessDaodao <694428809@qq.com>
2025-06-14 18:43:47 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -38,8 +38,9 @@ function initLoader() {
     if (!loader || !lines || lines.length === 0) return;
     
     const hasLoaded = sessionStorage.getItem('siteLoaded');
+    const hasSpecialLoader = document.querySelector('meta[name="ncbc-loader"][content="special"]');
     
-    if (!hasLoaded) {
+    if (!hasLoaded && !hasSpecialLoader) {
         lines.forEach(line => {
             line.style.visibility = 'hidden';
         });
@@ -70,6 +71,41 @@ function initLoader() {
         setTimeout(() => {
             showLines(0);
         }, 500);
+    } else if (hasSpecialLoader) {
+        loaderContent.innerHTML = `
+            <div class="loader-line" data-delay="99999999999999999"><span style="color: rgb(255, 65, 65);">错误: 404 Not Found</span></div>
+        `;
+
+        const quickLines = loaderContent.querySelectorAll('.loader-line');
+        quickLines.forEach(line => {
+            line.style.visibility = 'hidden';
+        });
+        
+        function showQuickLines(index) {
+            if (index < quickLines.length) {
+                quickLines[index].style.visibility = 'visible';
+                
+                const delay = quickLines[index].getAttribute('data-delay') ? 
+                    parseInt(quickLines[index].getAttribute('data-delay')) : 200;
+                
+                setTimeout(() => {
+                    showQuickLines(index + 1);
+                }, delay);
+            } else {
+                setTimeout(() => {
+                    loaderContent.innerHTML = '';
+                    
+                    setTimeout(() => {
+                        loader.style.display = 'none';
+                        document.body.style.display = 'block';
+                    }, 500);
+                }, 1000);
+            }
+        }
+
+        setTimeout(() => {
+            showQuickLines(0);
+        }, 300);
     } else {
         loaderContent.innerHTML = `
             <div class="loader-line" >NCBC2025@web ~$ <span style="color: rgb(137, 255, 130);">load</span> <span style="opacity: 0.5;">RnJpZW5kc2hpcCBpcyBNYWdpYwpEYW9kYW8gJiBSYWt1IElua3lldHRhIQ==</span></div>
