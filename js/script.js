d8c2e9897c837da76f6268e325a195b38f0ce2b0
JessDaodao <694428809@qq.com>
2025-09-19 16:52:24 +0800

    继续完善样式

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('DreamLandCon Web');
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
